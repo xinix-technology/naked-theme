e5ed3cef8585b5797cfe5b6ba83c6b751411da3c
Ganesha <reekoheek@gmail.com>
2014-05-26 12:28:08 +0700

    introduced page information filters

--- src/Xinix/Theme/NakedTheme.php
+++ src/Xinix/Theme/NakedTheme.php
@@ -3,6 +3,7 @@
 namespace Xinix\Theme;
 
 use \Bono\Theme\Theme;
+use \ROH\Util\Inflector;
 
 class NakedTheme extends Theme
 {
@@ -11,9 +12,39 @@ class NakedTheme extends Theme
     {
         parent::__construct($config);
 
+        $app = \Bono\App::getInstance();
+
         $d = explode(DIRECTORY_SEPARATOR.'src', __DIR__);
         $this->addBaseDirectory($d[0], 5);
 
         $this->resolveAssetPath('vendor/img');
+
+
+        $appConfig = $app->config('application');
+
+        $app->filter('page.title', function ($key) use ($app, $appConfig) {
+            if (isset($app->controller)) {
+                $module = Inflector::humanize($app->controller->clazz);
+                return $key .' - '. $module;
+            }
+            return $key;
+        });
+
+        $app->filter('page.header', function ($key) use ($appConfig) {
+            if (isset($appConfig['title'])) {
+                return $appConfig['title'];
+            }
+            return $key;
+        });
+
+        $app->filter('page.subheader', function ($key) use ($app, $appConfig) {
+            if (isset($app->controller)) {
+                return Inflector::humanize($app->controller->clazz);
+            }
+            if (isset($appConfig['subtitle'])) {
+                return $appConfig['subtitle'];
+            }
+            return $key;
+        });
     }
 }
